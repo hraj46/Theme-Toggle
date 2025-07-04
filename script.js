407bf99a381e0bf257949ccede96582b8d15fc92
Himanshu Raj <rajhimanshu7452@gmail.com>
2025-07-04 13:11:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const themeBtn = document.querySelector("#themeBtn");
+
+const savedTheme = localStorage.getItem("theme");
+document.body.className = "savedTheme";
+
+themeBtn.addEventListener("click", ()=> {
+    if(document.body.classList.contains("dark")){
+        document.body.className = "light"
+        localStorage.setItem("theme", "light");
+    } else {
+        document.body.className = "dark"
+        localStorage.setItem("theme", "dark");
+    }
+})
