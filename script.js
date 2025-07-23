5b07780916658c5d50fed74217d659a3e2bfe38a
Himanshu Raj <rajhimanshu7452@gmail.com>
2025-07-23 23:36:13 +0530

    Updated

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const themeBtn = document.querySelector("#themeBtn");
+const themeBtn = document.getElementById("themeBtn");
 
+// Load saved theme
 const savedTheme = localStorage.getItem("theme");
-document.body.className = "savedTheme";
+document.body.className = savedTheme;
 
-themeBtn.addEventListener("click", ()=> {
-    if(document.body.classList.contains("dark")){
-        document.body.className = "light"
-        localStorage.setItem("theme", "light");
-    } else {
-        document.body.className = "dark"
-        localStorage.setItem("theme", "dark");
-    }
-})
+themeBtn.addEventListener("click", () => {
+  if (document.body.classList.contains("dark")) {
+    document.body.className = "light";
+    localStorage.setItem("theme", "light");
+  } else {
+    document.body.className = "dark";
+    localStorage.setItem("theme", "dark");
+  }
+});
